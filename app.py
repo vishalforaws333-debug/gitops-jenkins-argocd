47212fa97595b007f413ff709c891b543bf97617
vishalforaws333-debug <vishalforaws333@gmail.com>
2026-06-08 06:52:25 +0000

    Version 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "GitOps CI/CD Working - Build Version"
+    return "GitOps CI/CD Working - Build Version 2"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
